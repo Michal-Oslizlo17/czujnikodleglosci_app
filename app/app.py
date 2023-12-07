ef63d4f62e5188e3278511059b31dba4c3b10d16
Michael Oslizlo <119759146+VermiNew@users.noreply.github.com>
2023-12-07 15:46:02 +0100

    Dodałem przyciski i dodałem moduł tkinter.

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ import serial
 import matplotlib.pyplot as plt
 from collections import deque
 import time
+import tkinter as tk
+from tkinter import ttk
 
 # Parametry do konfiguracji portu szeregowego
 COM_PORT = 'COM3'
@@ -11,6 +13,9 @@ BAUD_RATE = 9600
 MAX_POINTS = 50  # Maksymalna liczba punktów na wykresie
 PLOT_DELAY = 100
 PROGRAM_DELAY = 100
+
+# Globalne zmienne
+is_running = False
 
 # Inicjalizacja portu szeregowego
 ser = serial.Serial(COM_PORT, BAUD_RATE)
@@ -31,23 +36,46 @@ def update_plot(new_data):
     plt.draw()
     plt.pause(PLOT_DELAY)
 
-try:
-    while True:
-        # Odczyt danych z portu szeregowego
-        odczyt = ser.readline().decode('utf-8').strip()
+# Funkcje obsługujące zdarzenia przycisków
+def toggle_plotting():
+    global is_running
+    is_running = not is_running
+    if is_running:
+        start_button["state"] = "disabled"
+        stop_button["state"] = "normal"
+        root.after(0, start_plotting)
+    else:
+        start_button["state"] = "normal"
+        stop_button["state"] = "disabled"
 
-        # Przetwarzanie i aktualizacja wykresu
+def start_plotting():
+    if is_running:
         try:
-            odczyt_liczba = int(odczyt)
-            print(odczyt_liczba)
-            update_plot(odczyt_liczba)
-            time.sleep(PROGRAM_DELAY)  # Dodaj opóźnienie (np. 0.1 sekundy)
-        except ValueError as e:
-            print(f"Błąd przetwarzania danych: {e}")
+            odczyt = ser.readline().decode('utf-8').strip()
+            try:
+                odczyt_liczba = int(odczyt)
+                print(odczyt_liczba)
+                update_plot(odczyt_liczba)
+                root.after(PROGRAM_DELAY, start_plotting)
+            except ValueError as e:
+                print(f"Błąd przetwarzania danych: {e}")
+        except KeyboardInterrupt:
+            pass
 
+# Tworzenie interfejsu graficznego
+root = tk.Tk()
+root.title("Serial Plotter")
+
+start_button = ttk.Button(root, text="Start", command=toggle_plotting)
+start_button.pack(side=tk.LEFT, padx=10)
+
+stop_button = ttk.Button(root, text="Stop", command=toggle_plotting, state="disabled")
+stop_button.pack(side=tk.LEFT, padx=10)
+
+try:
+    root.mainloop()
 except KeyboardInterrupt:
-    # Obsługa przerwania przez użytkownika (Ctrl+C)
-    print("Przerwano przez użytkownika.")
+    pass
 finally:
     # Zamykanie portu szeregowego
     ser.close()
